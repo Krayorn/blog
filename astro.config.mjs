152bf2026772fbeee8cdfaf12d888124045bb7bb
Krayorn <nathael.arki@golem.ai>
2024-01-04 20:55:49 +0100

    board game post now with images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from 'astro/config';
-import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.krayorn.com/",
   trailingSlash: 'always',
-  integrations: [preact(), sitemap()],
   integrations: [
     sitemap({
       filter: (page) => !page.includes('draft'),
-    })
+    }),
+    mdx(),
   ],
 });
