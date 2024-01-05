3f444d34a2a0a57a7df8f92b376443db683c9e15
Krayorn <nathael.arki@golem.ai>
2024-01-05 10:20:48 +0100

    reorg boad game draft

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,10 +2,10 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'Astro Learner | Blog',
-    description: 'My journey learning Astro',
+    title: 'Krayorn | Blog',
+    description: 'A blog containing my thoughts and reflections on AI, Software, Board Games and whatever else finds it\'s way in my mind.',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.{md,mdx}')),
     customData: `<language>en-us</language>`,
   });
 }
