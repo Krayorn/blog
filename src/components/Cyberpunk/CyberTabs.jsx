6da76f0d1c52cafb354b1544b0d24f56503f5024
Krayorn <nath.arki36@gmail.com>
2024-11-08 22:32:08 +0100

    complete button form

--- src/components/Cyberpunk/CyberTabs.jsx
+++ src/components/Cyberpunk/CyberTabs.jsx
@@ -11,9 +11,12 @@ const CyberTabs = ({ data }) => {
 		<Tabs.List className="flex flex-col w-1/2" aria-label="Manage your account">
 			{data.map(tab => (
 				<div className="outer-border h-[67px] w-[302px] bg-[#d9453f] mt-2" >
-					<Tabs.Trigger  className="tabTrigger pl-2 relative top-px left-px flex flex-col justify-center h-[65px] w-[300px]  bg-[#7c2626] text-left" key={tab.title} value={tab.title}>
-						<span>{tab.title}</span>
-						<span className="text-[#fcc33c]" >Status: Completed</span>
+					<Tabs.Trigger className="tabTrigger relative top-px left-px flex h-[65px] w-[300px]  bg-[#7c2626] text-left" key={tab.title} value={tab.title}>
+						<div className="h-full mx-2 border-x-2 border-l-black border-r-[#d9453f] " />
+						<div className="flex flex-col mt-2" >
+							<span>{tab.title}</span>
+							<span className="text-[#fcc33c]" >Status: Completed</span>
+						</div>
 					</Tabs.Trigger>
 				</div>
 			))}
