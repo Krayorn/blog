69fec98f93b56e02b65f457268e25ea418d0402d
Krayorn <nath.arki36@gmail.com>
2024-11-09 00:28:49 +0100

    Button v1 complete

--- src/components/Cyberpunk/CyberTabs.jsx
+++ src/components/Cyberpunk/CyberTabs.jsx
@@ -1,22 +1,29 @@
 import * as Tabs from "@radix-ui/react-tabs";
 import "./CyberTabs.css"
 
-// .TabsTrigger[data-state="active"] {
-// 	color: violet;
-// }
-
 const CyberTabs = ({ data }) => {
-    return (
-        <Tabs.Root className="flex" defaultValue={data[0].title}>
+	return (
+        <Tabs.Root className="flex tabsroot" defaultValue={data[0].title}>
 		<Tabs.List className="flex flex-col w-1/2" aria-label="Manage your account">
 			{data.map(tab => (
-				<div className="outer-border h-[67px] w-[302px] bg-[#d9453f] mt-2" >
-					<Tabs.Trigger className="tabTrigger relative top-px left-px flex h-[65px] w-[300px]  bg-[#7c2626] text-left" key={tab.title} value={tab.title}>
-						<div className="h-full mx-2 border-x-2 border-l-black border-r-[#d9453f] " />
-						<div className="flex flex-col mt-2" >
+				<div className="outer-border h-[69px] w-[304px] bg-neonr mt-2" >
+					<Tabs.Trigger className="tabTrigger relative top-[2px] left-[2px] flex items-center h-[65px] w-[300px] bg-red text-left uppercase" key={tab.title} value={tab.title}>
+						<div className="h-full mx-2 border-x-2 border-l-black border-r-neonr " />
+						
+						{
+							tab.kind === 'web' && <Globe className="fill-none stroke-white stroke-1" />
+						}
+						{
+							tab.kind === 'app' && <App className="fill-none stroke-white stroke-1" />
+						}
+						{
+							tab.kind === 'contest' && <Contest className="fill-none stroke-white stroke-1" />
+						}
+						<div className="flex flex-col mt-1 ml-2" >
 							<span>{tab.title}</span>
-							<span className="text-[#fcc33c]" >Status: Completed</span>
+							<span className="text-neony text-sm" >Status: {tab.status}</span>
 						</div>
+						<div className="hidden highlight ml-auto h-full w-4 shadow-neon bg-neony" ></div>
 					</Tabs.Trigger>
 				</div>
 			))}
@@ -29,5 +36,25 @@ const CyberTabs = ({ data }) => {
 	</Tabs.Root>
     )
 }
+
+
+const Globe = ({ className }) => {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className={className} ><circle cx="12" cy="12" r="10"/><path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"/><path d="M2 12h20"/></svg>
+	)
+}
+
+const App = ({ className }) => {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className={className}><rect x="2" y="4" width="20" height="16" rx="2"/><path d="M10 4v4"/><path d="M2 8h20"/><path d="M6 4v4"/></svg>
+	)
+}
+
+const Contest = ({ className }) => {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"  className={className}><path d="m12 8 6-3-6-3v10"/><path d="m8 11.99-5.5 3.14a1 1 0 0 0 0 1.74l8.5 4.86a2 2 0 0 0 2 0l8.5-4.86a1 1 0 0 0 0-1.74L16 12"/><path d="m6.49 12.85 11.02 6.3"/><path d="M17.51 12.85 6.5 19.15"/></svg>
+	)
+}
+
 
 export default CyberTabs
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				neony: "#ffdb4b",
+				neonr: "#d9453f",
+				red: "#7c2626"
+			},
+			boxShadow: {
+				'neon': '0 0 10px 0 #ffdb4b',
+			}
+		},
 	},
 	plugins: [],
 }
