b8394501eaa1276de21c476875eb94908b50909a
Krayorn <nath.arki36@gmail.com>
2025-10-20 22:35:28 +0200

    upgrade astro 5, battle report mobile not good

---
+++ src/content.config.ts
@@ -0,0 +1,87 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const turnSchema = (image) => z.object({
+    secondary_missions: z.array(z.string()),
+    content: z.array(
+      z.union([
+        z.object({ type: z.literal('image'), src: image(), alt: z.string() }),
+        z.object({ type: z.literal('text'), content: z.string() })
+      ])
+    ),
+    score: z.array(z.object({ source: z.string(), value: z.number() }))
+});
+
+const battleReportCollection = defineCollection({
+  loader: glob({ pattern: '**/*.json', base: './src/content/battle_report' }),
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    date: z.string(),
+    description: z.string(),
+    game_data: z.object({
+      points: z.number(),
+      attacker: z.object({
+        name: z.string(),
+        faction: z.string(),
+        detachment: z.string(),
+        list: z.array(
+          z.union([
+            z.object({
+              name: z.string(),
+              equipment: z.array(z.string()),
+              points: z.number()
+            }),
+            z.object({
+              name: z.string(),
+              number: z.number(),
+              equipment: z.array(z.string()),
+              points: z.number()
+            })
+          ])
+        )
+      }),
+      defender: z.object({
+        name: z.string(),
+        faction: z.string(),
+        detachment: z.string(),
+        list: z.array(
+          z.union([
+            z.object({
+              name: z.string(),
+              equipment: z.array(z.string()),
+              points: z.number()
+            }),
+            z.object({
+              name: z.string(),
+              number: z.number(),
+              equipment: z.array(z.string()),
+              points: z.number()
+            })
+          ])
+        )
+      }),
+      primary_mission: z.string(),
+      deployment: z.string()
+    }),
+    rounds: z.array(
+        z.object({
+            attacker: turnSchema(image),
+            defender: turnSchema(image)
+        }),
+    )
+  }),
+});
+
+const cardCollection = defineCollection({
+  loader: glob({ pattern: '**/*.json', base: './src/content/cards' }),
+})
+
+const blogColletion = defineCollection({
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
+})
+
+export const collections = {
+  battle_report: battleReportCollection,
+  cards: cardCollection,
+  blog: blogColletion,
+};
--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -1,9 +1,9 @@
 import { getCollection } from "astro:content";
 
 export const getPosts = async () => {
-    return (await getCollection('blog')).filter(post => !post.slug.includes('/fr')).map(post => ({...post, slug: post.data.perma != undefined ? post.data.perma : post.slug}))
+    return (await getCollection('blog')).filter(post => !post.id.includes('/fr')).map(post => ({...post, slug: post.data.perma != undefined ? post.data.perma : post.id}))
 }
 
 export const getTranslatedPosts = async () => {
-    return (await getCollection('blog')).filter(post => post.slug.includes('/fr')).map(post => ({...post, slug: post.data.perma != undefined ? post.data.perma : post.slug}))
+    return (await getCollection('blog')).filter(post => post.id.includes('/fr')).map(post => ({...post, slug: post.data.perma != undefined ? post.data.perma : post.id}))
 }
