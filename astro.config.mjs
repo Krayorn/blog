a1c162a5aa06e3885ecd9fd7f69c2b8a570b97af
Krayorn <nath.arki36@gmail.com>
2025-11-18 10:09:31 +0100

    redirect posts to /

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,5 +17,6 @@ export default defineConfig({
   redirects: {
     '/posts/bouties-on-goals/': '/posts/bounties-on-goals/',
     '/posts/corrompus/': '/posts/corrompus-crown/',
+    '/posts/': '/',
   }
 });
